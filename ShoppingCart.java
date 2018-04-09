396271f0c5a49bfeb1b8fdff0d5f17f9a65a6350
Oscar Evertsson <oscar.evertsson@live.com>
2018-04-09 11:21:05 +0200

    Implemented basic file.

---
+++ ShoppingCart.java
@@ -0,0 +1,26 @@
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+
+    }
+
+    private void printBalance() {
+
+    }
+
+    private void printProductList() {
+
+    }
+
+    private void printRequest() {
+
+    }
+
+    private void printBalanceAfterPurchase() {
+
+    }
+
+    private void handleInput(String input) {
+
+    }
+}
